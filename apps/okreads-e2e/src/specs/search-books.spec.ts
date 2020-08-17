6c53cb774e5f726ed6bc81503804be926ad114bd
rakesh <kallurirakesh15@gmail.com>
2020-08-17 14:27:35 -0400

    e2e

--- apps/okreads-e2e/src/specs/search-books.spec.ts
+++ apps/okreads-e2e/src/specs/search-books.spec.ts
@@ -16,7 +16,25 @@ describe('When: Use the search feature', () => {
     const items = await $$('[data-testing="book-item"]');
     expect(items.length).to.be.greaterThan(1, 'At least one book');
   });
+  it('Then: undo the book', async () => {
+    await browser.get('/');
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement($('tmo-root'), 'okreads')
+    );
+    const form = await $('form');
+    const input = await $('input[type="search"]');
+    const book = await $('[data-testing="want-read-book-item"]');
+    const undobook = await $('[data-testing="undo-book-item"]');
+    // check pdfdata to make sure it got reflected
 
+    await input.sendKeys('javascript');
+    await form.submit();
+    await book.click();
+    await undobook.click();
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement(book, 'Want to Read')
+    );
+  });
   xit('Then: I should see search results as I am typing', async () => {
     await browser.get('/');
     await browser.wait(
